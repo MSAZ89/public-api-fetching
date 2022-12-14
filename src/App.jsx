8c35944a57ad83743d2193ec19880d448e7a9bb4
MSAZ89 <michaelsimonaz@gmail.com>
2022-12-14 15:37:47 -0700

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
               }, [])
               .map((entry, index) => (
                 <button
-                  className="bg-slate-700 text-white font-bold p-2 rounded text-sm"
+                  className="bg-slate-700 transition-all text-white font-bold p-2 rounded text-sm focus:bg-slate-200 focus:text-black focus:border-2 focus:border-slate-700 border-2 border-slate-700"
                   key={index}
                   onClick={() => setFilterName(entry)}
                 >
@@ -52,31 +52,20 @@ function App() {
           }
         </>
       </div>
+      <p className="font-bold tracking-tighter text-xl uppercase">
+        {filter.length > 0
+          ? filter.length + " results for " + filterName
+          : null}
+      </p>
       {storedData !== [] ? (
         <div className="grid grid-cols-3 gap-4 divide-y">
-          {filter.length > 0
-            ? filter.length + " results for " + filterName
-            : null}
           {filter != ""
             ? filter.map((entry, index) => (
                 <div
                   className="bg-slate-800 px-8 py-8 rounded text-white my-1"
                   key={index}
                 >
-                  <p className="text-xl font-bold mb-4">{entry.API}</p>
-                  <div className="bg-slate-500 space-4 text-white py-2 rounded mb-4">
-                    <p className="mb-4 text-md m-4">
-                      <span className="font-bold">Note: </span>
-                      {entry.Description}
-                    </p>
-                    <p className="mb-4 text-md m-4">
-                      <span className="font-bold">Category:</span>{" "}
-                      {entry.Category}
-                    </p>
-                    <p className="mb-4 text-md m-4">
-                      <span className="font-bold">Auth:</span> {entry.Auth}
-                    </p>
-                  </div>
+                  <p className="text-xl font-bold mb-2">{entry.API}</p>
                   <a
                     className="tracking-tight text-slate-400 hover:text-slate-300 transition-all"
                     target={"_blank"}
@@ -85,6 +74,28 @@ function App() {
                   >
                     {entry.Link}
                   </a>
+                  <div className="bg-slate-700 space-4 text-white py-2 rounded mt-4">
+                    <p className="mb-4 text-md m-4">{entry.Description}</p>
+                    <p className="mb-4 text-sm m-4">
+                      <span className="font-bold">Category:</span>{" "}
+                      {entry.Category}
+                    </p>
+                    <p className="mb-4 text-sm m-4">
+                      <span className="font-bold">Auth:</span> {entry.Auth}
+                    </p>
+                    <p className="mb-4 text-sm m-4">
+                      <span className="font-bold">HTTPS:</span>{" "}
+                      {entry.HTTPS.toString()}
+                    </p>
+                    <p className="text-sm m-4">
+                      <span className="font-bold">CORS:</span>{" "}
+                      {entry.Cors === "yes" ? (
+                        <span className="text-orange-400">Yes</span>
+                      ) : (
+                        <span className="text-lime-400">No</span>
+                      )}
+                    </p>
+                  </div>
                 </div>
               ))
             : null}
